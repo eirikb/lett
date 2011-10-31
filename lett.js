558c36b7ba8a4b586814ab8cd9a914e46dbcc1e5
Eirik Brandtzæg <eirikb@eirikb.no>
2011-11-01 00:00:11 +0100

    Recursive functions are fun

--- lett.js
+++ lett.js
@@ -1,32 +1,53 @@
-var alles = {};
+function build(parent, code) {
+    var i, s, l, r, i1 = code.indexOf(':'),
+    i2 = code.indexOf('{');
+
+    if (i1 < 0 && i2 < 0) {
+        return assign(code.trim());
+    }
+
+    if (i1 < 0) {
+        i1 = Number.MAX_VALUE;
+    }
+    if (i2 < 0) {
+        i2 = Number.MAX_VALUE;
+    }
+
+    if (i1 < i2) {
+        i = code.indexOf(':');
+        var a = {};
+        parent[code.slice(0, i).trim()] = a;
+        parent = a;
+        a = build(parent, code.slice(i + 1));
+        return a;
+    } else {
+        var o = {};
+        code.slice(code.indexOf('{') + 1, code.indexOf('}')).split(',').forEach(function(c) {
+            build(o, c);
+        });
+        return o;
+    }
+}
+
+function assign(b) {
+    if (b.match(/^('|").*('|")$/)) {
+        b = b.slice(1, b.length - 1);
+    } else if (b.match(/true/i)) {
+        b = true;
+    } else if (b.match(/false/i)) {
+        b = false;
+    }else if (b.match(/.*(.*)$/ig)) {
+     b = 'EXEC(' + b + ')';  
+    } else {
+        b = parseInt(b, 10);
+    }
+    return b;
+}
 
 exports.run = function(code) {
-    code.split('{').forEach(function(p) {
-        p.split('}').forEach(function(p) {
-            p.split(',').forEach(function(p) {
-                p = p.trim();
-                if ( !! p) {
-                    var s = p.split(':'),
-                    a = s[0];
-                    b = s[1].trim();
-
-                    if (b.match(/^('|").*('|")$/)) {
-                        b = b.slice(1, b.length - 1);
-                    } else if (b.match(/true/i)) {
-                        b = true;
-                    } else if (b.match(/false/i)) {
-                        b = false;
-                    } else {
-                        b = parseInt(b, 10);
-                    }
-                    alles[a] = b;
-                }
-            });
-        });
-    });
-
+    var alles = {};
+    build(alles, code);
     console.log(alles);
-
     return 'Oh, hai';
 };
 
