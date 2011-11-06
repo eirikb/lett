5f92c3895befe1dc4a6dbccafe99dc919ba36437
Eirik Brandtzæg <eirikb@eirikb.no>
2011-11-06 23:54:03 +0100

    WORKS! But not as I want... thinking of another way to parse

--- lett.js
+++ lett.js
@@ -1,37 +1,48 @@
 var lettlib = require('./lib.js');
 
-function build(code) {
+function build(code, level) {
     var o = {};
 
     while (code.length > 0) {
-        var clips, sides = splitFirst(code, [':', '{']),
-        result = sides[sides.length - 1],
-        current = sides.length > 1 ? path(sides[0].split('.'), o) : {};
+        var clips, lines, name, urrent, sides = splitFirst(code, [':', '{']),
+        result = sides[sides.length - 1];
 
-        clips = clip(result, ['{', '}']);
-        console.log(clips);
-        if (clips) {
-            current = result.slice(clips[0] + 1, clips[1]);
-            code = result.slice(clips[1] + 1).trim();
-            console.log(code);
-            code = '';
+        if (sides) {
+            clips = clip(result, ['{', '}']);
+            if (clips.length > 0) {
+                code = result.slice(clips[1] + 1).trim();
+                result = result.slice(clips[0] + 1, clips[1]);
+            } else {
+                lines = splitFirst(result, '\n');
+                if (lines.length > 1) {
+                    result = lines[0].trim();
+                    code = lines[1];
+                } else {
+                    result = sides[1];
+                    code = '';
+                }
+            }
+            ass(sides[0].replace(/\n/g, '').trim(), o, result);
         } else {
-            current = result;
-            code = '';
+            return assign(code.trim());
         }
     }
-    console.log(o)
+    return o;
 }
 
-function path(path, parent) {
-    path.forEach(function(name) {
-        if (typeof parent[name] === 'object') {
-            parent = parent[name];
+function ass(path, parent, obj) {
+    var paths = path.split('.');
+    paths.forEach(function(name, i) {
+        if (i < paths.length - 1) {
+            if (typeof parent[name] === 'object') {
+                parent = parent[name];
+            } else {
+                parent = parent[name] = {};
+            }
         } else {
-            parent = parent[name] = {};
+            parent[name] = build(obj);
         }
     });
-    return parent;
 }
 
 function splitFirst(code, splits) {
@@ -41,14 +52,19 @@ function splitFirst(code, splits) {
             min = i;
         }
     });
-    return [code.slice(0, min), code.slice(min + 1)].filter(function(s) {
-        s = s.trim();
-        return s.length > 0 ? s: false;
-    });
+    if (min) {
+        return [code.slice(0, min), code.slice(min + 1)].filter(function(s) {
+            s = s.trim();
+            return s.length > 0 ? s: false;
+        });
+    } else {
+        return false;
+    }
 }
 
 function indexOfAll(code, targets) {
-    var c, i, imsy = false, indexes = [],
+    var c, i, imsy = false,
+    indexes = [],
     outside = ["'", '"'];
 
     for (i = 0; i < code.length; i++) {
@@ -67,7 +83,7 @@ function indexOfAll(code, targets) {
 function clip(code, targets) {
     var level = 0;
     return indexOfAll(code, targets).filter(function(i, j) {
-        level += code.charAt(i) === '{' ? 1 : -1;
+        level += code.charAt(i) === '{' ? 1: - 1;
         return j === 0 || level === 0;
     });
 }
