16d353b491ae6f362816de821d4eb2c70fa56fe4
Eirik Brandtzæg <eirikb@eirikb.no>
2012-08-30 21:03:03 +0000

    Wrapping in description - skippable (pending)

--- test/builder.js
+++ test/builder.js
@@ -1,127 +1,129 @@
 var assert = require('assert');
 var lett = require('../lett.js');
 
-describe('assignment', function() {
-    it('should return 42 when its the last statement', function() {
-        assert.equal(42, lett('42'));
-        assert.equal(42, lett('1 42'));
-        assert.equal(42, lett('1 2 3 42'));
-        assert.equal(42, lett('a 2 a 42'));
+describe('builder', function() {
+    describe('assignment', function() {
+        it('should return 42 when its the last statement', function() {
+            assert.equal(42, lett('42'));
+            assert.equal(42, lett('1 42'));
+            assert.equal(42, lett('1 2 3 42'));
+            assert.equal(42, lett('a 2 a 42'));
+        });
+
+        it('should set var a to 3', function() {
+            assert.equal(3, lett('a 3 a'));
+            assert.equal(3, lett('a 7 a 3 a'));
+        });
+
+        // They are litterals! I promise!
+        it('should set true and false to ... true and false', function() {
+            assert.equal(true, lett('true'));
+            assert.equal(false, lett('false'));
+        });
     });
 
-    it('should set var a to 3', function() {
-        assert.equal(3, lett('a 3 a'));
-        assert.equal(3, lett('a 7 a 3 a'));
+    describe('objects', function() {
+        it('should set a to 7', function() {
+            assert.equal(7, lett('{a 7}').a);
+            assert.equal(7, lett('test {a 7} test').a);
+            assert.equal(7, lett('{test {a 7}}').test.a);
+            assert.notEqual(7, lett('{test {a 7}}').a);
+        });
+
+        it('should support deeper objects', function() {
+            assert.equal(42, lett('{a { b { c 42 }}').a.b.c);
+            assert.notEqual(42, lett('{a { b { c 42 }}').c);
+        });
+
+        it('should support objects of any type', function() {
+            var res = lett('{a { b "test" c 7}}');
+            assert.equal('test', res.a.b);
+            assert.equal(7, res.a.c);
+        });
     });
 
-    // They are litterals! I promise!
-    it('should set true and false to ... true and false', function() {
-        assert.equal(true, lett('true'));
-        assert.equal(false, lett('false'));
-    });
-});
+    describe('arrays', function() {
+        it('should support assignment og arrays', function() {
+            assert.equal(2, lett('[1 2 3]')[1]);
+        });
 
-describe('objects', function() {
-    it('should set a to 7', function() {
-        assert.equal(7, lett('{a 7}').a);
-        assert.equal(7, lett('test {a 7} test').a);
-        assert.equal(7, lett('{test {a 7}}').test.a);
-        assert.notEqual(7, lett('{test {a 7}}').a);
-    });
-
-    it('should support deeper objects', function() {
-        assert.equal(42, lett('{a { b { c 42 }}').a.b.c);
-        assert.notEqual(42, lett('{a { b { c 42 }}').c);
+        it('should support arrays of any type', function() {
+            var res = lett('[1 2 {a 42} {a {b "test"}} 7]');
+            assert.equal(2, res[1]);
+            assert.equal(42, res[2].a);
+            assert.equal("test", res[3].a.b);
+            assert.equal(7, res[res.length - 1]);
+        });
     });
 
-    it('should support objects of any type', function() {
-        var res = lett('{a { b "test" c 7}}');
-        assert.equal('test', res.a.b);
-        assert.equal(7, res.a.c);
-    });
-});
+    describe('calls', function() {
+        it('should return 42 when adding 21 with 21', function() {
+            assert.equal(42, lett('+(21 21)'));
+        });
 
-describe('arrays', function() {
-    it('should support assignment og arrays', function() {
-        assert.equal(2, lett('[1 2 3]')[1]);
-    });
+        it('should support n amount of arguments', function() {
+            assert.equal(42, lett('+(10 10 10 12)'));
+        });
 
-    it('should support arrays of any type', function() {
-        var res = lett('[1 2 {a 42} {a {b "test"}} 7]');
-        assert.equal(2, res[1]);
-        assert.equal(42, res[2].a);
-        assert.equal("test", res[3].a.b);
-        assert.equal(7, res[res.length - 1]);
-    });
-});
+        it('should support nesting of calls', function() {
+            assert.equal(42, lett('+(10 10 +(10 12))'));
+            assert.equal(42, lett('+(10 10 +(10 -(22 10)))'));
+        });
 
-describe('calls', function() {
-    it('should return 42 when adding 21 with 21', function() {
-        assert.equal(42, lett('+(21 21)'));
+        it('should support any types as arguments', function() {
+            assert.equal(42, lett('a 10 b 20 c 10 +(a b c 2)'));
+        });
     });
 
-    it('should support n amount of arguments', function() {
-        assert.equal(42, lett('+(10 10 10 12)'));
+    describe('chaining', function() {
+        it('should support chaining of objects', function() {
+            assert.equal(42, lett('a { b 42 } a.b'));
+            assert.equal(42, lett('a { b { c { d 42 }}} a.b.c.d'));
+        });
+
+        it('should support directly on objects', function() {
+            assert.equal(42, lett('a { b { c { d 42 }}}.b.c.d'));
+        });
     });
 
-    it('should support nesting of calls', function() {
-        assert.equal(42, lett('+(10 10 +(10 12))'));
-        assert.equal(42, lett('+(10 10 +(10 -(22 10)))'));
+    describe('functions', function() {
+        it('should create a function returning 42', function() {
+            assert.equal(42, lett('<,42>')());
+        });
+
+        it('should accept arguments', function() {
+            assert.equal(42, lett('<a,a>')(42));
+        });
+
+        it('should execute function calls within the function', function() {
+            assert.equal(42, lett('<a b, +(a b) >')(21, 21));
+        });
+
+        it('should be able to execute directly after declaration', function() {
+            //  TODO: This fails!  - Why?
+            //  assert.equal(42, lett('<a b, +(a b)>(21 21)'));
+        });
+
+        it('should handle larger bodies', function() {
+            assert.equal(42, lett('<,a 42 a>')());
+            assert.equal(42, lett('<a,a>')(42));
+            assert.equal(42, lett('test <a,a> test(42)'));
+        });
     });
 
-    it('should support any types as arguments', function() {
-        assert.equal(42, lett('a 10 b 20 c 10 +(a b c 2)'));
-    });
-});
-
-describe('chaining', function() {
-    it('should support chaining of objects', function() {
-        assert.equal(42, lett('a { b 42 } a.b'));
-        assert.equal(42, lett('a { b { c { d 42 }}} a.b.c.d'));
+    describe('getters', function() {
+        it('should support getters by calls with no name', function() {
+            // TODO: Not implemented
+            //assert.equal(42, lett('a {b 42} (a "b")'));
+        });
     });
 
-    it('should support directly on objects', function() {
-        assert.equal(42, lett('a { b { c { d 42 }}}.b.c.d'));
-    });
-});
-
-describe('functions', function() {
-    it('should create a function returning 42', function() {
-        assert.equal(42, lett('<,42>')());
-    });
-
-    it('should accept arguments', function() {
-        assert.equal(42, lett('<a,a>')(42));
-    });
-
-    it('should execute function calls within the function', function() {
-        assert.equal(42, lett('<a b, +(a b) >')(21, 21));
-    });
-
-    it('should be able to execute directly after declaration', function() {
-        //  TODO: This fails!  - Why?
-        //  assert.equal(42, lett('<a b, +(a b)>(21 21)'));
-    });
-
-    it('should handle larger bodies', function() {
-        assert.equal(42, lett('<,a 42 a>')());
-        assert.equal(42, lett('<a,a>')(42));
-        assert.equal(42, lett('test <a,a> test(42)'));
-    });
-});
-
-describe('getters', function() {
-    it('should support getters by calls with no name', function() {
-        // TODO: Not implemented
-        //assert.equal(42, lett('a {b 42} (a "b")'));
-    });
-});
-
-describe('object paramters', function() {
-    it('can be sent into <,> as arguments', function() {
-        var a = {
-            b: 42
-        };
-        assert.equal(42, lett('b', a));
+    describe('object paramters', function() {
+        it('can be sent into <,> as arguments', function() {
+            var a = {
+                b: 42
+            };
+            assert.equal(42, lett('b', a));
+        });
     });
 });
--- test/corelib.js
+++ test/corelib.js
@@ -1,44 +1,45 @@
 var assert = require('assert');
 var lett = require('../lett.js');
 
-describe('if statements', function() {
-    it('should be evaluated correctly', function() {
-        assert.equal(42, lett('if(true 42)'));
-        assert.equal(42, lett('if(false 7 42)'));
-    });
-});
-
-describe('corelib properties', function() {
-    it('should handle addition', function() {
-        assert.equal(42, lett('+(10 10 20 2'));
+describe('corelib', function() {
+    describe('if statements', function() {
+        it('should be evaluated correctly', function() {
+            assert.equal(42, lett('if(true 42)'));
+            assert.equal(42, lett('if(false 7 42)'));
+        });
     });
 
+    describe('corelib properties', function() {
+        it('should handle addition', function() {
+            assert.equal(42, lett('+(10 10 20 2'));
+        });
 
-    it('should handle subtraction', function() {
-        assert.equal(0, lett('-(42 10 10 20 2'));
-    });
+        it('should handle subtraction', function() {
+            assert.equal(0, lett('-(42 10 10 20 2'));
+        });
 
-    it('should handle dividing', function() {
-        assert.equal(42, lett('/(84 2)'));
-    });
+        it('should handle dividing', function() {
+            assert.equal(42, lett('/(84 2)'));
+        });
 
-    it('should handle multiplication', function() {
-        assert.equal(42, lett('*(21 2)'));
-    });
+        it('should handle multiplication', function() {
+            assert.equal(42, lett('*(21 2)'));
+        });
 
-    it('should handle string concationation', function() {
-        assert.equal('It is known, Khaleesi', lett('str("It is known" "," " Khaleesi")'));
-    });
+        it('should handle string concationation', function() {
+            assert.equal('It is known, Khaleesi', lett('str("It is known" "," " Khaleesi")'));
+        });
 
-    it('should handle greater than', function() {
-        assert.equal(true, lett('gt(42 7)'));
-    });
+        it('should handle greater than', function() {
+            assert.equal(true, lett('gt(42 7)'));
+        });
 
-    it('should handle lesser than', function() {
-        assert.equal(true, lett('lt(42 100)'));
-    });
+        it('should handle lesser than', function() {
+            assert.equal(true, lett('lt(42 100)'));
+        });
 
-    it('should handle equality', function() {
-        assert.equal(true, lett('eq(42 42)'));
+        it('should handle equality', function() {
+            assert.equal(true, lett('eq(42 42)'));
+        });
     });
 });
