35aad6c36f8663735b551192a406e85eb01f09bf
Eirik Brandtzæg <eirikb@eirikb.no>
2012-08-30 21:19:05 +0000

    clean

---
+++ test/parser.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var parser = require('../parser.js');
+
+describe('parser', function() {
+    it('should not halt on errorous strings', function(done) {
+        // Not sure if this is correct syntax
+        this.timeout(500);
+        parser.parse('"');
+        done();
+    });
+});
