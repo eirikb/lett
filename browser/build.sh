77e506fd3413a130d6cc1fdfc609002f34028034
Eirik Brandtzæg <eirikb@eirikb.no>
2012-09-13 23:45:24 +0000

    removed builder.js from build.sh

--- browser/build.sh
+++ browser/build.sh
@@ -1,4 +1,4 @@
 top="lett = (function(){var exports={};module={};"
-lett=$(cat ../corelib.js ../parser.js ../builder.js ../lett.js)
+lett=$(cat ../corelib.js ../parser.js ../lett.js)
 bottom="parser=exports;builder=exports;return module.exports;})()"
 echo "$top $lett $bottom" | grep -vE 'require' | uglifyjs > lett.min.js
