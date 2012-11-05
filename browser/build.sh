dd363e2f94ac74a3bbb05ff5f7c40f74a6c9098c
Eirik Brandtzæg <eirikb@eirikb.no>
2012-11-05 21:13:11 +0000

    using src folder

--- browser/build.sh
+++ browser/build.sh
@@ -1,4 +1,4 @@
 top="lett = (function(){var exports={};module={};"
-lett=$(cat ../corelib.js ../parser.js ../lett.js)
+lett=$(cat ../src/corelib.js ../src/parser.js ../src/lett.js)
 bottom="parser=exports;builder=exports;return module.exports;})()"
 echo "$top $lett $bottom" | grep -vE 'require' | uglifyjs > lett.min.js
